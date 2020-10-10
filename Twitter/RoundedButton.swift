79f30a03338c129c2f87316f1747141734c9e7ee
Julio Aguilar <julio.aguilar@upr.edu>
2020-10-09 23:55:49 -0400

    finished everything with bonuses

---
+++ Twitter/RoundedButton.swift
@@ -0,0 +1,40 @@
+//
+//  RoundedButton.swift
+//  Twitter
+//
+//  Created by Julio Leonardo Aguilar Colon on 10/9/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class RoundButton : UIButton{
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        sharedInit()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        sharedInit()
+    }
+
+    override func prepareForInterfaceBuilder() {
+        sharedInit()
+    }
+
+    func sharedInit() {
+        refreshCorners(value: cornerRadius)
+    }
+
+    func refreshCorners(value: CGFloat) {
+        layer.cornerRadius = value
+    }
+
+    @IBInspectable var cornerRadius: CGFloat = 15 {
+        didSet {
+            refreshCorners(value: cornerRadius)
+        }
+    }
+}
